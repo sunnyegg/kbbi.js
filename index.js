f80cf48bb3218244e4bebdecb1225b9caa03b0df
Adila <adilatresna21@gmail.com>
2020-01-18 19:07:26 +0700

    Update: Hasil Pencarian

--- index.js
+++ index.js
@@ -14,31 +14,90 @@ const cari = async (katakunci) => {
 
 // let's start
 (async () => {
+  // jika input kosong
+  if (!argv.length) {
+    return console.log('Tidak boleh kosong.');
+  }
+
   try {
-    const hasil = await cari(argv[0]); // fetch data dari url berdasarkan input
+    // fetch data dari url berdasarkan input
+    const hasil = await cari(argv[0]);
 
-    const $ = cheerio.load(hasil); // init cheerio
+    // init cheerio
+    const $ = cheerio.load(hasil);
 
-    const hasilPencarian = $('h2').text(); // hasil kata
-    const hasilDefinisi = []; // hasil definisi dari kata
+    // hasil pencarian kata
+    const hasilPencarian = $('h2').text();
+    // hasil definisi kata
+    const hasilDefinisi = [];
+    // hasil jenis kata
+    const jenisKataDefinisi = [];
 
-    $('ol')
-      .find('li')
-      .each((index, element) => {
-        const definisi = $(element)
-          .text()
-          .trim();
+    // jika entri tidak ditemukan
+    const tidakDitemukan = $('.body-content')
+      .clone()
+      .children('h4')
+      .first()
+      .text()
+      .trim();
 
-        hasilDefinisi[index] = { definisi }; // masukkan hasil ke array
-      });
+    if (tidakDitemukan === 'Entri tidak ditemukan.') {
+      return console.log(tidakDitemukan);
+    }
 
-    console.log('');
+    // proses mengambil data
+    $('ol > li').each((i, el) => {
+      const definisi = $(el)
+        .clone()
+        .children()
+        .remove()
+        .end()
+        .text();
+
+      const jenis = $(el)
+        .clone()
+        .children()
+        .first()
+        .text()
+        .trim()
+        .split(/\W+/);
+
+      $(el)
+        .clone()
+        .find('span')
+        .each((i, el) => {
+          const definisiJenis = $(el).attr('title');
+
+          jenisKataDefinisi.push(definisiJenis);
+        });
+
+      $(el)
+        .find('font:nth-child(1)')
+        .remove();
+
+      const contoh = $(el)
+        .children()
+        .text()
+        .trim();
+
+      // masukkan hasil ke array
+      hasilDefinisi[i] = { jenis, definisi, contoh };
+    });
+
+    // hilangkan duplikat
+    const filter = Array.from(new Set(jenisKataDefinisi));
+
+    // tampilkan hasil
     console.log(hasilPencarian);
     hasilDefinisi.map((value, index) => {
-      console.log(index + 1 + ': ' + value.definisi);
+      console.log(`${index + 1}: (${value.jenis}) ${value.definisi} ${value.contoh}`);
     });
     console.log('');
+    filter.map((val) => {
+      console.log(val);
+    });
   } catch (err) {
+    // jika error terjadi
     console.log(err);
   }
 })();
